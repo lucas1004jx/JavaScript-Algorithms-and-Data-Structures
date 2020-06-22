d7ef0d8e7cb23988488b914fca21c653adc18047
lucas1004jx <lucas1004jx@gmail.com>
2020-06-22 22:09:27 +0200

    implement pop method

--- single-linked-list.js
+++ single-linked-list.js
@@ -24,10 +24,36 @@ class SingleLinkedList{
         this.length++;
         return this;
     }
+
+    pop(){
+        if(!this.head) return undefined;
+        if(this.length === 1) {
+            const tail = this.tail;
+            this.head = null;
+            this.tail = null;
+            this.length--;
+            return tail;
+        }
+        let currentNode = this.head;
+        let newTail = currentNode;
+        while(currentNode.next){
+            newTail = currentNode;
+            currentNode = currentNode.next;
+        }
+        newTail.next = null;
+        this.tail = newTail;
+        this.length--;
+        return currentNode;
+    }
 }
 
 const list = new SingleLinkedList();
 list.push('hi');
 list.push('there');
 list.push('lucas');
+console.log('list-->',list );
+list.pop();
+list.pop();
+list.pop();
+
 console.log('list-->',list );
