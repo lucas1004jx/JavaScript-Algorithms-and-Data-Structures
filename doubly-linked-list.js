27283551d3ce7be20aa2e2272c20c833271cbdd2
lucas1004jx <lucas1004jx@gmail.com>
2020-07-05 23:45:52 +0200

    implement pop and shif method

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -26,12 +26,49 @@ class DoublyLinkedList {
         this.length++;
         return this;
     }
+
+    pop(){
+        if(!this.head) return undefined;
+        const removedNode = this.tail;
+        if(this.length === 1) {
+            this.head= null;
+            this.tail = null;
+        }else{
+            this.tail = removedNode.prev;
+            this.tail.next = null;
+            removedNode.prev = null;
+        }
+        this.length--;
+        return removedNode;
+    }
+
+    shift(){
+        if(!this.head) return undefined;
+        const removedNode =  this.head;
+        if(this.length === 1){
+            this.head = null;
+            this.tail = null;
+        }else{
+            this.head = removedNode.next;
+            this.head.prev = null;
+            removedNode.next = null;
+        }
+        this.length--;
+        return removedNode;
+    }
 }
 
 const list = new DoublyLinkedList();
 
 list.push(1);
- list.push(2);
-// list.push(3);
+list.push(2);
+list.push(3);
+list.push(4);
+list.push(5);
+
+//list.pop();
+//list.pop();
+
+list.shift();
 
 console.log('list-->',list);
