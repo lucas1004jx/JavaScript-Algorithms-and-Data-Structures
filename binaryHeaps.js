c30ff77d9c0bdf4165d7df6c8e4c1e12aa12ad3b
lucas1004jx <lucas1004jx@gmail.com>
2020-07-30 22:59:33 +0200

    binary heap extractMax method

--- binaryHeaps.js
+++ binaryHeaps.js
@@ -22,6 +22,45 @@ class BinaryHeaps {
         const orderedArr = bubbleUp(this.values,val,this.values.length-1);
         return orderedArr;
     }
+
+    extractMax(){
+        if(this.values.length === 0) return null;
+        if(this.values.length === 1) return this.values[0];
+        const length = this.values.length;
+        [this.values[0],this.values[length-1]] = [this.values[length-1],this.values[0]];
+        const max = this.values.pop();
+
+        const sinkDown = (arr,value,index) => {
+            const lefChildIndex = 2*index+1;
+            const rightChildIndex = 2*index+2;
+            const leftChild = arr.length - 1 >= lefChildIndex ? arr[lefChildIndex] : null;
+            const rightChild = arr.length - 1 >= rightChildIndex ?  arr[rightChildIndex] : null;
+            if(!leftChild && !rightChild) return arr;
+            let maxValue;
+            let maxIndex;
+            if(rightChild) {
+                if(leftChild > rightChild){
+                    maxValue = leftChild;
+                    maxIndex = lefChildIndex;
+                }else if(leftChild < rightChild){
+                    maxValue = rightChild;
+                    maxIndex = rightChildIndex;
+                }
+            }else{
+                maxValue = leftChild;
+                maxIndex = lefChildIndex;
+            }
+            
+
+            if(value > maxValue) return arr;
+            [arr[index],arr[maxIndex]] = [arr[maxIndex],arr[index]];
+            sinkDown(arr,arr[maxIndex],maxIndex);
+        } 
+
+        sinkDown(this.values,this.values[0],0);
+        return this.values;
+    }
+    
 }
 
 const heap = new BinaryHeaps();
