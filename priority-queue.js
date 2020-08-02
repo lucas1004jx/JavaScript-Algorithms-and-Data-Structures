e0701573a7d7117d936f13b54737652b91cd615c
lucas1004jx <lucas1004jx@gmail.com>
2020-08-02 12:01:17 +0200

    priority queue enqueue method

--- priority-queue.js
+++ priority-queue.js
@@ -9,4 +9,26 @@ class PriorityQueue {
     constructor(){
         this.values = [];
     }
+
+    enqueue(val, priority){
+        const newNode = new Node(val, priority);
+        this.values.push(newNode);
+        if(this.values.length === 1) return this.values;
+        
+        const bubbleUp= (node,idx) =>{
+            if(idx === 0) return this.values;
+            const parentIdx = Math.floor((idx - 1)/2);
+            const parentNode = this.values[parentIdx];
+            const parentPriority = this.values[parentIdx].priority;
+            if(parentPriority< node.priority) return this.values;
+            [this.values[parentIdx], this.values[idx]] = [node,parentNode];
+            return bubbleUp(node,parentIdx);
+        }
+        const length = this.values.length;
+        bubbleUp(this.values[length - 1],length-1);
+
+        return this.values;
+    }
 }
+
+const queue = new PriorityQueue();
