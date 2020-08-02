178872f75aae9f958c11014777f4d9fc3f1bdc2a
lucas1004jx <lucas1004jx@gmail.com>
2020-08-02 11:15:33 +0200

    create priorityQueue class

---
+++ priority-queue.js
@@ -0,0 +1,12 @@
+class Node {
+    constructor(val,priority){
+        this.value = val;
+        this.priority = priority;
+    }
+}
+
+class PriorityQueue {
+    constructor(){
+        this.values = [];
+    }
+}
